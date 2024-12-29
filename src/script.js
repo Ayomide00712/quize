42b7593357f411d2d5cffdd034e551d00d10a7fe
Alkassim <usmanalkassim01@gmail.com>
2024-12-28 18:31:02 -0800

    modified

---
+++ src/script.js
@@ -0,0 +1,22 @@
+// document.getElementById("nextButton").addEventListener("click", () => {
+//     // You would put logic here to decide where to go next.
+//     // For example, based on the current page.
+//    if (window.location.pathname.endsWith("index.html")) {
+//        window.location.href = "file2.html";
+//    } else if (window.location.pathname.endsWith("file2.html")) {
+//        window.location.href = "file3.html";
+//    } else if (window.location.pathname.endsWith("file3.html")) {
+//        window.location.href = "file4.html";
+//    } else if (window.location.pathname.endsWith("page3.html")) {
+//         window.location.href = "index.html"; // Go back to the start
+//    }
+// });
+
+// document.getElementById("prevButton").addEventListener("click", () => {
+//    history.back(); // Use the browser's back button functionality
+//});
+
+// Track the current step
+//let currentStep = 1;
+
+// Navigate forward
